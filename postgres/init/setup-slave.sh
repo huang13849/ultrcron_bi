230f5dd1afc85850e82ea1a2a4bd7be0043186bc
Frank Huang <huang13849@hotmail.com>
2024-06-07 15:39:39 +0800

    postgre slave  uodate

--- postgres/init/setup-slave.sh
+++ postgres/init/setup-slave.sh
@@ -10,13 +10,10 @@ rm -rf "$PGDATA"/*
 # Perform base backup from master
 PGPASSWORD=masterpassword pg_basebackup -h 209.141.34.146 -D "$PGDATA" -U replicator -vP --wal-method=stream
 
-# Create recovery configuration file
-cat <<EOF > "$PGDATA/recovery.conf"
-standby_mode = 'on'
-primary_conninfo = 'host=209.141.34.146 port=5432 user=replicator password=replicatorpassword'
-primary_slot_name = 'replication_slot'
-trigger_file = '/tmp/promote_to_master'
-EOF
+touch "$PGDATA/standby.signal"
+
+echo "primary_conninfo = 'host=209.141.34.146 port=5432 user=replicator password=replicatorpassword'" >> "$PGDATA/postgresql.conf"
+echo "primary_slot_name = 'replication_slot'" >> "$PGDATA/postgresql.conf"
 
 # Start PostgreSQL server
 pg_ctl start
